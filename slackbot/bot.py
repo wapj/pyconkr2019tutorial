4cab2d301afc242e18f3afe2aa42502038f793dc
andy <andy@podotree.com>
2019-08-09 15:22:00 +0900

    슬랙봇 예제 수정

--- slackbot/bot.py
+++ slackbot/bot.py
@@ -1,19 +1,21 @@
 import os
 import random
-import ssl
 import time
 from functools import wraps
 
 import aiohttp
-import certifi
 import requests
 import slack
 from bs4 import BeautifulSoup
 
+# 파이썬 슬랙클라이언트를 사용함
+# https://github.com/slackapi/python-slackclient
+# 아래의 라이브러리들이 필요
+# pip3 install slackclient requests beautifulsoup4 aiohttp
+
 
 def main():
-    ssl_context = ssl.create_default_context(cafile=certifi.where())
-    rtm_client = slack.RTMClient(token=os.environ["BOT_ACCESS_TOKEN"], ssl=ssl_context)
+    rtm_client = slack.RTMClient(token=os.environ["BOT_ACCESS_TOKEN"])
     print("bot START!")
     print(register)
     res = rtm_client.start()
@@ -51,20 +53,26 @@ async def download_img(url):
                 print(res.text)
 
 
+async def choice_gag():
+    gags = [["길을 가다 나무를 주웠다를 3글자로?", "정답 : 우드득"], ["고양이가 지옥에가면?", "정답 : 헬로키티"]]
+
+    return random.choice(gags)
+
+
 @command("아재개그")
 async def azae_gag(client, data):
     """아재개그를 보여줍니다."""
-
-    await send(client, data, "길을 가다 나무를 주웠다를 3글자로?")
+    gag = await choice_gag()
+    await send(client, data, gag[0])
     await send(client, data, text="3")
     await send(client, data, text="2")
     await send(client, data, text="1")
-    await send(client, data, text="정답 : 우드득")
+    await send(client, data, text=gag[1])
 
 
 @command("짤")
 async def zzal(client, data):
-    """구글에서 짤을 검색해서 보여줍니다. aiohttp를 사용합니다."""
+    """네이버에서 짤을 검색해서 보여줍니다. aiohttp를 사용합니다."""
     channel_id = data.get("channel")
     text = data.get("text")
     search = text[3:]
@@ -142,7 +150,6 @@ async def control_message(**payload):
 
 if __name__ == "__main__":
     main()
-
 
 # !대여, !반납 을 인식하도록 하게 해서 어떤 유저가 어떤 책을 빌리고 반납했는지, 확인할 수 있는 슬랙봇을 만들어보자.
 # DB는 기본적으로는 필요없지만, 파일에는 json 형식으로 써서 import/export 가 가능하도록 해보자.
